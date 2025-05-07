d3d13823f54458857877869e65748ccf2c4f81f6
jacoposantini-gft <jacopo.santini@gft.com>
2025-05-07 15:13:28 +0200

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/Comment.java

--- src/main/java/com/scalesec/vulnado/Comment.java
+++ src/main/java/com/scalesec/vulnado/Comment.java
@@ -52,10 +52,8 @@ public class Comment {
       }
       cxn.close();
     } catch (Exception e) {
-      e.printStackTrace();
       System.err.println(e.getClass().getName()+": "+e.getMessage());
     } finally {
-      return comments;
     }
   }
 
@@ -67,9 +65,7 @@ public class Comment {
       pStatement.setString(1, id);
       return 1 == pStatement.executeUpdate();
     } catch(Exception e) {
-      e.printStackTrace();
     } finally {
-      return false;
     }
   }
 
